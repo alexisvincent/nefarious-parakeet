c591487fbf13bcde1b0956e161ba6d3e59f1f264
Alexis Vincent <alexisjohnvincent@gmail.com>
2015-09-15 16:14:09 +0200

    Added Line filter

--- src/InputParser.py
+++ src/InputParser.py
@@ -1,12 +1,45 @@
 dictionary = dict()
-point1 = 0
-point2 = 0
 
-def lineFilter(item):
-    return True
+from vectors import *
+  
+# Given a line with coordinates 'start' and 'end' and the
+# coordinates of a point 'pnt' the proc returns the shortest 
+# distance from pnt to the line and the coordinates of the 
+# nearest point on the line.
+#
+# 1  Convert the line segment to a vector ('line_vec').
+# 2  Create a vector connecting start to pnt ('pnt_vec').
+# 3  Find the length of the line vector ('line_len').
+# 4  Convert line_vec to a unit vector ('line_unitvec').
+# 5  Scale pnt_vec by line_len ('pnt_vec_scaled').
+# 6  Get the dot product of line_unitvec and pnt_vec_scaled ('t').
+# 7  Ensure t is in the range 0 to 1.
+# 8  Use t to get the nearest location on the line to the end
+#    of vector pnt_vec_scaled ('nearest').
+# 9  Calculate the distance from nearest to pnt_vec_scaled.
+# 10 Translate nearest back to the start/end line. 
+# Malcolm Kesson 16 Dec 2012 http://www.fundza.com/vectors/point2line/index.html`
+def pnt2line(pnt, start, end):
+    line_vec = vector(start, end)
+    pnt_vec = vector(start, pnt)
+    line_len = length(line_vec)
+    line_unitvec = unit(line_vec)
+    pnt_vec_scaled = scale(pnt_vec, 1.0/line_len)
+    t = dot(line_unitvec, pnt_vec_scaled)    
+    if t < 0.0:
+        t = 0.0
+    elif t > 1.0:
+        t = 1.0
+    nearest = scale(line_vec, t)
+    dist = distance(nearest, pnt_vec)
+    nearest = add(nearest, start)
+    return (dist, nearest)
 
-def addGridItem(item):
-    if lineFilter(item):
+def lineFilter(start, end, point, r):
+    return pnt2line(point, start, end)[0] <= r
+
+def addGridItem(item, p1, p2, r):
+    if lineFilter(p1, p2, (item[1], item[2], item[3]), r):
         dictionary[item[0]] = (item[1], item[2], item[3]) 
 
 def parseNumber(num): 
@@ -14,24 +47,19 @@ def parseNumber(num):
         return float(num[0] + num[1:].replace('-', 'e-'))
     return float(num)
 
-def parseLine(line):
+def parseLine(line, p1, p2, r):
     if (line.startswith('GRID')):
         addGridItem((
             int(line[8:16]), 
             parseNumber(line[24:32]), 
             parseNumber(line[32:40]), 
             parseNumber(line[40:48])
-        ))
+        ), p1, p2, r)
 
-def getData(fileName, p1, p2):
-
-    point1 = p1
-    point2 = p2
-
+def getData(fileName, p1, p2, r):
+    dictionary.clear()
     with open(fileName, "r") as file:
         for line in file:
-            parseLine(line)
+            parseLine(line, p1, p2, r)
     file.close()
     return dictionary
-
-
--- test/InputExample.py
+++ test/InputExample.py
@@ -3,6 +3,10 @@ sys.path.append('../src/')
 
 from InputParser import getData;
 
-dataDictionary = getData('./Data/Input.pch', 0, 0)
-print dataDictionary
+p1 = (0, 0, 0) # Line point 1
+p2 = (0, 1, 1) # Line point 2
+r = 10 # Distance of data point from line
 
+dataDictionary = getData('./Data/Input.pch', p1, p2, r)
+
+print dataDictionary
